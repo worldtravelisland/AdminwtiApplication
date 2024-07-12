7e72ba40ab6be81594e28dd21796a2093bef4f87
WTI <160489178+worldtravelisland@users.noreply.github.com>
2024-07-12 12:59:11 +0530

    Add project files.

---
+++ MYSQLWTIApplication/Controllers/Master/CountryMasterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MYSQLWTIApplication.Controllers.Master
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryMasterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllCountries()
+        {
+            return Ok("SuccessFull");
+        }
+    }
+}
